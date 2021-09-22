e1fa7dc21c2f03df8a195558a231a7337daf5686
Stainislav <swatogor@yandex.ru>
2021-09-22 21:27:02 +0300

    changed to adding special characters

--- rename.js
+++ rename.js
@@ -1,35 +1,29 @@
 const fs = require('fs');
 const { readdir } = require('fs/promises');
 const { rename } = require('fs/promises');
+let { specialCharacters } = require('./specialCharacters.js');
 
-const dirPath = 'files';
-
-let fileNames = [
-    "abc+.jpg",
-    "abc=.jpg",
-    "abc*.jpg",
-    "abc___.jpg",
-    "abc---.jpg",
-    "abc123.jpg",
-    "abc .jpg",
-    "   .jpg",
-    "hello.jpg",
-    "=abc.jpg",
-]
-
-async function renameFiles(directoryPath) {
+async function addSpecialCharactersToFileNames(directoryPath) {
     try {
-        const files = await readdir(directoryPath);
-        files.forEach(async function(fileName, index) {
-            if (fileNames[index] === undefined) return;
-            const oldPath = dirPath + '/' + fileName;
-            const newPath = dirPath + '/' + fileNames[index];
-            await rename(oldPath, newPath);
+        const filesArray = await readdir(directoryPath);
+        filesArray.forEach((fileName, index) => {
+            if (specialCharacters[index] === undefined) return;
+
+            const splittedFileName = fileName.split('.');
+            const fileNameWithoutExtension = splittedFileName[0];
+            const fileExtension = splittedFileName[1];
+
+            const oldPath = directoryPath + '/' + fileName;
+            const newPath = directoryPath + '/' + fileNameWithoutExtension + specialCharacters[index] + '.' + fileExtension;
+            rename(oldPath, newPath);
         });
       } catch (err) {
         console.error(err);
       }
 }
 
-renameFiles(dirPath);
-console.log('Renaming is done. Check your files folder');
+const dirPath = 'files';
+
+addSpecialCharactersToFileNames(dirPath).then(() => {
+    console.log('Renaming is done. Check your files folder');
+});
