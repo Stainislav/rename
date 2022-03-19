93f09de9462665b791708618d7999813bbf90157
Stainislav <swatogor@yandex.ru>
2022-03-19 23:34:46 +0300

    refactor: delete redundant files and folders
    delete redundant files and folders
    
    FILES-DOWNLOADING

--- filesDownloading.js
+++ filesDownloading.js
@@ -1,7 +1,6 @@
 import { createClient } from 'pexels';
 import { downloadPhotos } from './photosDownloading.js';
 import { downloadVideos } from './videosDownloading.js';
-import { addSpecialCharactersToFileNames } from './filesRenaming.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -18,24 +17,12 @@ const videosNumber = 4;
 console.log('Photos downloading has started.');
 
 downloadPhotos(client, query, photosDirPath, photosNumber).then(() => {
-    setTimeout(() => {
-        console.log('Photos renaming has started.');
-        addSpecialCharactersToFileNames(photosDirPath).then(() => {
-            console.log('Photos renaming is done.');
-        });
-    }, 10000);
     console.log('Photos downloading is done.');
 });
 
 console.log('Videos downloading has started.');
 
 downloadVideos(client, query, videosDirPath, videosNumber).then(() => {
-    setTimeout(() => {
-        console.log('Videos renaming has started.');
-        addSpecialCharactersToFileNames(videosDirPath).then(() => {
-            console.log('Videos renaming is done.');
-        });
-    }, 20000);
     console.log('Videos downloading is done.');
 });
 
